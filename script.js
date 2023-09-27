f6bbe6d3950112e4e656efe783b99adbfae1d639
vikiDesigns <victorw135@gmail.com>
2023-09-27 13:48:47 +0300

    initial comiit

---
+++ script.js
@@ -0,0 +1,35 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const rec = new SpeechRecognition();
+
+rec.lang = "en-US";
+rec.continuous = false;
+
+rec.onresult = function (e) {
+  const acceptedColors = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "pink",
+    "brown",
+    "purple",
+    "orange",
+    "black",
+    "white",
+  ];
+
+  for (let i = e.resultIndex; i < e.results.length; i++) {
+    const script = e.results[i][0].transcript.toLowerCase().trim();
+
+    if (acceptedColors.includes(script)) {
+      document.body.style.backgroundColor = script;
+      document.querySelector("h1").style.color = script;
+    } else {
+      alert("Please say another color");
+    }
+  }
+};
+
+rec.start();
